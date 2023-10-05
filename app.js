74ec69f888a0ccff016bc0b27603d92754292a42
carlospianoforte <carlospianiforte@gmail.com>
2023-10-04 19:19:18 -0500

    first commit

---
+++ app.js
@@ -0,0 +1,26 @@
+const nombreInput = document.getElementById("nombre");
+const fechaInput = document.getElementById("fecha");
+const horaInput = document.getElementById("hora");
+const agregarButton = document.getElementById("agregar");
+const citasContainer = document.getElementById("citas");
+
+agregarButton.addEventListener("click", () => {
+    const nombre = nombreInput.value;
+    const fecha = fechaInput.value;
+    const hora = horaInput.value;
+
+
+    if (nombre && fecha) {
+        const citaCard = document.createElement("div");
+        citaCard.classList.add("cita-card");
+        citaCard.innerHTML = `<h2>${nombre}</h2> <p>Fecha: ${fecha}</p> <p2>Hora: ${hora}</p2>`;
+        citasContainer.appendChild(citaCard);
+
+        nombreInput.value = "";
+        fechaInput.value = "";
+        horaInput.value = "";
+
+    } else {
+        alert("Por favor, complete todos los campos.");
+    }
+});
