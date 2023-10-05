eb2bbb6486a95ef91089b18a73e29f646b798c88
carlospianoforte <carlospianiforte@gmail.com>
2023-10-04 21:34:30 -0500

    final

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ agregarButton.addEventListener("click", () => {
     if (nombre && fecha && hora) {
         const citaCard = document.createElement("div");
         citaCard.classList.add("cita-card");
-        citaCard.innerText = `<h2>${nombre}</h2> <p>Fecha: ${fecha}</p> <p2>Hora: ${hora}</p2>`;
+        citaCard.innerHTML = `<h2>Cita de ${nombre}</h2> <p>Fecha: ${fecha}</p> <p2>Hora: ${hora}</p2>`;
         citasContainer.appendChild(citaCard);
 
         nombreInput.value = "";
@@ -24,3 +24,27 @@ agregarButton.addEventListener("click", () => {
         alert("Por favor, complete todos los campos.");
     }
 });
+
+const $tiempo=document.querySelector('.tiempo'),
+$fecha= document.querySelector('.fecha');
+
+function Relojdigital(){
+    let f=new Date(),
+    dia= f.getDate(),
+    mes= f.getMonth()+1,
+    anio= f.getFullYear(),
+    diaSemana=f.getDay();
+
+    dia= ('0'+dia).slice(-2);
+    mes=('0'+mes).slice(-2)
+
+    let timeString= f.toLocaleTimeString();
+    $tiempo.innerHTML=timeString;
+
+    let semana=['DOMINGO','LUNES','MARTES','MIERCOLES','JUEVES','VIERNES','SABADO'];
+    let showSemana= (semana[diaSemana])
+    $fecha.innerHTML = `${showSemana} ${dia}-${mes}-${anio}`
+}
+setInterval(() =>{
+    Relojdigital()
+},1000);
